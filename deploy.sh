c604659abe65a8342b6728a436f4997b89c08767
Thomas Khalil <thomas@khalilt.com>
2016-06-14 16:17:16 +0200

    cleanup

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 set -e
 set -o pipefail
 
-[ "$AWS_S3_BUCKET" ] || echo "Missing s3 bucket";exit 1
-[ "$AWS_ACCESS_KEY" ] || echo "Missing s3 access";exit 1
-[ "$AWS_SECRET_KEY" ] || echo "Missing s3 secret";exit 1
+[ "$AWS_S3_BUCKET" ] || exit 1
+[ "$AWS_ACCESS_KEY" ] || exit 1
+[ "$AWS_SECRET_KEY" ] || exit 1
 
 # upload the files to s3
-s3cmd sync --delete-removed -P . s3://$AWS_S3_BUCKET/
+s3cmd sync --delete-removed -P site/ s3://$AWS_S3_BUCKET/
